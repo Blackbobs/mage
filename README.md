c73342bf262900c6cb9739b7da88c34898235753
Black Bobs <ayodejiayodele350@gmail.com>
2023-09-08 16:08:02 +0100

    mvp

---
+++ README.md
@@ -0,0 +1,29 @@
+# Introduction
+
+Welcome to **Mage**. We are an E-commerce and social media platform focused on promoting and selling various forms of _art_. we aim to connect artist,collectors with the global online art community,providing them with a platform to showcase and sell their work. Our goal is to bring visibility to talented artist by leveraging _digital market strategies_ and creating an accessible avenue for art enthusiats to discover and purchase unique pieces.
+
+---
+
+## Features
+
+**User intreface**: We aim to Provide a great user interface for users,so as to make transactions easy and a great user experience for our users. Our developers are working really hard to create a great user friendly user interface.
+
+**Promotions and Visibility**: _Mage_ as a company has decided to help a great number of artist with their work to gain _visibility_ with their talent and show them to the community and also build a _career_ while doing what they love.
+
+**Social Media Platform**: Our developers have decided to integrate social platform into the web app,so the community can be interactive. You can decide to request a special great art from your favourite artist to be delivered to you. _Mage_ has decided to build connection between our users and community.
+
+**Auction**: There's this great feature we decided to call _auction_, you can decide as an artist to put your work up for auctioning and have people **BID** for your art designs with great prices. Another great feature our developers are working on is the ability of buyers to negiotiate the prices of art designs to thier satisfaction.
+
+---
+
+## Tech Stack
+
+- Html
+- Sass
+- JaveScript
+
+---
+
+## Anticipate
+
+We hope probably in the future we will integrate **Web3** into our platform for great _user interface,transparency,secure transaction,security_,e.t.c.
---
+++ public/js/main.js
@@ -0,0 +1,307 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+const productDom = document.querySelector(".product-case");
+const cartTotal = document.querySelector(".cart-total");
+const cartBtn = document.querySelector(".fa-cart-plus");
+const cartOverlay = document.querySelector(".cart-overlay");
+const closeBtn = document.querySelectorAll(".fa-times-circle");
+const profileBtn = document.querySelectorAll(".fa-user-circle");
+const profilePage = document.querySelector(".outer-profile");
+const cartDisplay = document.querySelector(".cart-display");
+const clearCartBtn = document.querySelector(".clear-cart-btn");
+const closeMenu = document.querySelector(".close-menu");
+const barMenu = document.querySelector(".fa-bars-staggered");
+const sideMenu = document.querySelector(".side-menu");
+const profileIcon = document.querySelector(".profile-link");
+const search = document.querySelector(".search");
+const checkoutBtn = document.querySelector(".checkout-btn");
+
+// Switch between tabs
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  this.classList.add("current");
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add("show");
+}
+
+// remove border from each tab
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("current"));
+}
+
+// remove the show class from each tab
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
+}
+
+// display menu
+function displayMenu() {
+  sideMenu.classList.toggle("show");
+}
+
+// display the profile page
+function displayProfile() {
+  profilePage.classList.add("show");
+}
+
+// Event Listeners
+barMenu.addEventListener("click", displayMenu);
+closeMenu.addEventListener("click", displayMenu);
+
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
+
+cartBtn.addEventListener("click", () => {
+  cartOverlay.classList.toggle("show");
+});
+
+closeBtn.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    close = e.target.parentElement.parentElement;
+    close.classList.remove("show");
+  })
+);
+
+profileBtn.forEach((btn) => btn.addEventListener("click", displayProfile));
+
+profileIcon.addEventListener("click", () => {
+  profilePage.classList.add("show");
+  displayMenu();
+});
+
+// Cart array
+let cart = [];
+// add to cart btn
+let buttonsDom = [];
+
+// fetch products
+class Products {
+  async getProducts(searchText) {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.items;
+      products = products.map((item) => {
+        const title = item.name_d;
+        const price = item.price;
+        const id = item.id;
+        return { title, price, id };
+      });
+      return products;
+    } catch (error) {}
+  }
+}
+
+class UI {
+  // Display the products to the Dom
+  displayProducts(products) {
+    let result = "";
+    products.forEach((product) => {
+      result += `
+          <div class="item">
+                <img src="./images/5eaf615122919-1.png" alt="charcoal protraits"/>
+                <div class="grp">
+                  <h4 class="name">John Doe</h4>
+                  <h4 class="categories">Abstract</h4>
+                </div>
+                <small class="title">${product.title}</small>
+                <div class="check">
+                  <small class="price">$${product.price}</small>
+                  <button href="#!" class="plus-cart" data-id=${product.id}>add to cart</button>
+                  
+                </div>
+          </div>
+      `;
+    });
+    productDom.innerHTML = result;
+  }
+
+  searchProducts(products) {
+    search.addEventListener("input", (e) => {
+      const searchText = e.target.value;
+
+      let matches = products.filter((product) => {
+        const regex = new RegExp(`^${searchText}`, "gi");
+        return product.title.match(regex);
+      });
+
+      if (searchText.length === 0) {
+        this.displayProducts(products);
+        // this.setupAPP();
+        // this.getBagButtons();
+        // this.addCartItem(item);
+        // matches = [];
+      }
+      // console.log(matches);
+      this.outputSearch(matches);
+    });
+  }
+
+  outputSearch(matches) {
+    this.displayProducts(matches);
+    // this.setupAPP();
+    this.getBagButtons();
+    // this.addCartItem(item);
+  }
+
+  // Addd to cartBtn functionality
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll(".plus-cart")];
+    buttonsDom = buttons;
+    buttons.forEach((button) => {
+      let id = button.dataset.id;
+      let inCart = cart.find((item) => item.id === id);
+      if (inCart) {
+        button.innerText = "In cart";
+        button.disabled = true;
+      }
+      button.addEventListener("click", (e) => {
+        e.target.innerText = "In Cart";
+        e.target.disabled = true;
+
+        // Add item to the cart
+        let cartItem = { ...Storage.getProducts(id) };
+        cart = [...cart, cartItem];
+
+        // Save cart in storage
+        Storage.saveCart(cart);
+
+        // Set cart total values
+        this.setCartValues(cart);
+        // Add Item to the cart Dom
+        this.addCartItem(cartItem);
+      });
+    });
+  }
+
+  // Cart Math
+  setCartValues(cart) {
+    let tempTotal = 0;
+    cart.map((item) => {
+      tempTotal += parseFloat(item.price);
+    });
+    cartTotal.innerText = `${tempTotal}`;
+  }
+
+  // Display the item to the Dom
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-items");
+    div.innerHTML = `
+    <img src="./images/5eaf615122919-1.png" alt="" />
+            <div class="cart-info">
+              <h2 class="author">john doe</h2>
+
+              <p class="title">${item.title}</p>
+              <h4>$${item.price}</h4>
+              <small class="description"
+                >Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eveniet provident vitae nihil. Inventore accusamus odit tenetur
+                ullam, doloremque totam animi.</small
+              >
+              <a href="#!" class="remove-item" data-id="${item.id}">remove</a>
+            </div>
+    `;
+    cartDisplay.appendChild(div);
+  }
+
+  // Setup the UI and funtionality on DomContentLoaded
+  setupAPP() {
+    cart = Storage.getCart();
+    this.setCartValues(cart);
+    this.populateCart(cart);
+  }
+
+  // Add every item in the cart to the cartDom
+  populateCart(cart) {
+    cart.forEach((item) => this.addCartItem(item));
+  }
+
+  // cart funtionality
+  cartLogic() {
+    // eventListener to clear cart
+    clearCartBtn.addEventListener("click", () => {
+      this.clearCart();
+    });
+
+    // remove individual item from the Dom
+    cartDisplay.addEventListener("click", (e) => {
+      if (e.target.classList.contains("remove-item")) {
+        let removeItem = e.target;
+        let id = removeItem.dataset.id;
+        cartDisplay.removeChild(removeItem.parentElement.parentElement);
+        this.removeItem(id);
+      }
+    });
+  }
+
+  // Clear cart logic
+  clearCart() {
+    let cartItems = cart.map((item) => item.id);
+    cartItems.forEach((id) => this.removeItem(id));
+
+    while (cartDisplay.children.length > 0) {
+      cartDisplay.removeChild(cartDisplay.children[0]);
+    }
+  }
+
+  // Remove item from the cart
+  removeItem(id) {
+    // filter the target id from cart storage
+    cart = cart.filter((item) => item.id !== id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+    // reset the add to cartBtn
+    let button = this.getSingleButton(id);
+    button.disabled = false;
+    button.innerHTML = "add to cart";
+  }
+
+  getSingleButton(id) {
+    return buttonsDom.find((button) => button.dataset.id === id);
+  }
+}
+
+class Storage {
+  // Save products in the storage
+  static saveProduct(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+
+  // Get products from the storage
+  static getProducts(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find((product) => product.id === id);
+  }
+
+  // save the cart into the storage
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
+  // Get items in the cart from storage
+  static getCart() {
+    return localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+
+  ui.setupAPP();
+
+  products
+    .getProducts()
+    .then((products) => {
+      ui.displayProducts(products);
+      ui.searchProducts(products);
+      Storage.saveProduct(products);
+    })
+    .then(() => {
+      ui.getBagButtons();
+      ui.cartLogic();
+    });
+});
