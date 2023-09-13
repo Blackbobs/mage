36f05be81e5724f162bfda2d6e5affe0f96bc4ab
Black Bobs <ayodejiayodele350@gmail.com>
2023-09-13 01:25:58 +0100

    setting up firebase

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4hkvtcR1OrLyYIRPR10ZWhdncF584rsI",
+  authDomain: "mage-5b9f9.firebaseapp.com",
+  projectId: "mage-5b9f9",
+  storageBucket: "mage-5b9f9.appspot.com",
+  messagingSenderId: "338442865042",
+  appId: "1:338442865042:web:c75da25ea80909534d594b",
+  measurementId: "G-XVN8KVCCK3",
+};
+
+initializeApp(firebaseConfig);
+
 const tabItems = document.querySelectorAll(".tab-item");
 const tabContentItems = document.querySelectorAll(".tab-content-item");
 const productDom = document.querySelector(".product-case");
